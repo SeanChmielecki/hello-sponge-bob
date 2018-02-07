e30b6375f8f3c15fe247368ef32f01770738d5d6
Sean Chmielecki <35748130+SeanChmielecki@users.noreply.github.com>
2018-02-06 19:09:14 -0500

    Add files via upload

---
+++ hello-sponge-bob/src/Exercise1.java
@@ -0,0 +1,7 @@
+
+public class Exercise1 {   // This is the main
+
+	public static void main(String[] args) {  // This is java's signiture. 
+		System.out.println("Hello\nSpongebob!"); /// This will print out Hello Spongebob.
+	}
+}
